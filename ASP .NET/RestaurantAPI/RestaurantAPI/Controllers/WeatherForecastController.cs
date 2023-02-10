d55ba55e3f0121e285ac8468d09c99394cacce1d
mk7obl <mkomsa7@gmail.com>
2023-02-10 16:16:22 +0100

    minor code clarity update

--- ASP .NET/RestaurantAPI/RestaurantAPI/Controllers/WeatherForecastController.cs
+++ ASP .NET/RestaurantAPI/RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -11,29 +11,21 @@ namespace RestaurantAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
+        private readonly IWeatherForecastService _service;
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
         {
             _logger = logger;
+            _service = service;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var results = _service.Get();
+            return results;
         }
     }
 }
