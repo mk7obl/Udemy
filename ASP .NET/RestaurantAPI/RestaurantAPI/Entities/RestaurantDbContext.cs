31b3351100a8accc465b56f50c521249b37c77e5
mk7obl <mkomsa7@gmail.com>
2023-02-18 19:16:45 +0100

    defined entities & connected to database

---
+++ ASP .NET/RestaurantAPI/RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI.Entities
+{
+
+    public class RestaurantDbContext : DbContext
+    {
+        //private string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=RestaurantDb;Trusted_Connection=True;";
+
+        private string _connectionString = "Server=.\\SQLExpress;Database=RestaurantDB;Trusted_Connection=Yes;";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+    }
+}
