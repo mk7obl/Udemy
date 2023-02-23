e8316991bd96f946bf232dab0ea5c8632859d95a
mk7obl <mkomsa7@gmail.com>
2023-02-23 12:38:26 +0100

    request-time middleware

---
+++ ASP .NET/RestaurantAPI/RestaurantAPI/Middleware/RequestTimeMiddleware.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using RestaurantAPI.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Middleware
+{
+    public class RequestTimeMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        private readonly Stopwatch _stopWatch;
+
+        public RequestTimeMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+            _stopWatch = new Stopwatch();
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            _stopWatch.Start();
+            await next.Invoke(context);
+            _stopWatch.Stop();
+
+
+            var elapsedTime = _stopWatch.ElapsedMilliseconds;
+
+            if (elapsedTime/1000 > 4)
+            {
+                var message = $"Request [{context.Request.Method}] at {context.Request.Path} took {elapsedTime} ms";
+
+                _logger.LogTrace(message);
+            }
+
+
+
+        }
+    }
+}
