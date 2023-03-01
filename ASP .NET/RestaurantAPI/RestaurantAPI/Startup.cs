f49a4e3d1b4013dab892e703e0ad80e6aba887fc
mk7obl <mkomsa7@gmail.com>
2023-03-01 23:16:07 +0100

    login fix

--- ASP .NET/RestaurantAPI/RestaurantAPI/Startup.cs
+++ ASP .NET/RestaurantAPI/RestaurantAPI/Startup.cs
@@ -41,6 +41,7 @@ namespace RestaurantAPI
             Configuration.GetSection("Authentication").Bind(authenticationSettings);
 
             services.AddSingleton(authenticationSettings);
+
             services.AddAuthentication(option =>
             {
                 option.DefaultAuthenticateScheme = "Bearer";
